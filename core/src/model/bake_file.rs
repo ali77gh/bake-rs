a9531383a32db141f7e9f6ab0704da7a7fc6e98f
Ali Ghahremani <alighahremani1377@gmail.com>
2024-08-29 19:24:48 +0330

    feat: global variables

--- core/src/model/bake_file.rs
+++ core/src/model/bake_file.rs
@@ -1,6 +1,7 @@
-use super::{dependency::Dependency, task::Task};
+use super::{dependency::Dependency, param::Param, task::Task};
 
 struct BakeFile {
+    global_env_vars: Vec<Param>,
     dependencies: Vec<Dependency>,
     tasks: Vec<Task>,
 }
