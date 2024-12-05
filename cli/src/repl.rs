565f461f57b0d9d678f273294cd6cac93af6bf88
Ali Ghahremani <alighahremani1377@gmail.com>
2024-12-05 11:19:16 +0330

    feat: reading bakefile again after user input in bake loop

--- cli/src/repl.rs
+++ cli/src/repl.rs
@@ -6,16 +6,8 @@ use crate::{capabilities::CLICapabilities, show_tasks::show_tasks};
 pub fn start_repl() {
     let caps = Rc::new(CLICapabilities::default());
 
-    let bake_caps = Rc::clone(&caps);
-    let bake = match BakeViewModel::new(bake_caps) {
-        Ok(x) => x,
-        Err(e) => {
-            println!("{}", e);
-            exit(1); // cleaner way to panic
-        }
-    };
-
     loop {
+        let bake = new_bake(&caps);
         show_tasks(bake.tasks());
         let task_name = match caps.ask_user("which task do you want to run (enter name or index)") {
             Some(x) => x,
@@ -25,13 +17,27 @@ pub fn start_repl() {
             }
         };
 
+        // read the bakefile again to get new changes happened during ask_user
+        let bake = new_bake(&caps);
+
         let task_name = task_name.trim();
         if let Err(e) = bake.run_task(task_name) {
-            CLICapabilities::default().message(Message::error(format!("{e}\n")));
-            CLICapabilities::default().message(Message::error(format!(
+            caps.message(Message::error(format!("{e}\n")));
+            caps.message(Message::error(format!(
                 "Task '{task_name}' failed to run\n"
             )));
         }
         println!("\n──────────Bake-Loop──────────\n");
+    }
+}
+
+fn new_bake(caps: &Rc<CLICapabilities>) -> BakeViewModel {
+    let bake_caps = Rc::clone(caps);
+    match BakeViewModel::new(bake_caps) {
+        Ok(x) => x,
+        Err(e) => {
+            println!("{}", e);
+            exit(1); // cleaner way to panic
+        }
     }
 }
